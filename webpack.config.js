3478917acfb5b735a567bb1a9b62deb8ad0d9990
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-06-20 23:55:05 +0530

    chore: webpack setup and eslinting

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+const NodemonPlugin = require('nodemon-webpack-plugin'); // Ding
+
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './api/server.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'server.js'
+  },
+  externals: nodeModules,
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+          // default value
+          formatter: 'stylish',
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new NodemonPlugin(), // Dong
+  ],
+}
