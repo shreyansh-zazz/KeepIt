674a9a3a312bed1a3285e0651bd0a69a0696c608
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-06-23 23:34:03 +0530

    feat: add mongodb connection configurations

---
+++ api/config/database.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const databaseHost = process.env.DATABASE_HOST || '127.0.0.1';
+const databseSRV = process.env.DATABASE_SRV ? '+srv' : '';
+const databasePort = process.env.DATABASE_PORT || 27017;
+const databaseName = process.env.DATABASE_NAME || 'keepit';
+const databaseAuth = process.env.DATABASE_USERNAME ? process.env.DATABASE_USERNAME.concat([':', process.env.DATABASE_PASSWORD, '@']) : '';
+const databaseConnectionAuth = process.env.AUTHENTICATION_DATABASE || 'admin';
+const databaseSSL = process.env.DATABASE_SSL || false;
+
+const databaseURL = `mongodb${databseSRV}://${databaseAuth}${databaseHost}:${databasePort}/${databaseName}?authSource=${databaseConnectionAuth}`;
+
+mongoose.connect(databaseURL, {
+  useNewUrlParser: true,
+  ssl: databaseSSL,
+})
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Database connected');
+  })
+  .catch((err) => {
+    throw err;
+  });
+
+export default mongoose.connection;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 import { ApolloServer } from 'apollo-server';
 
 import Modules from './modules/modules';
+import DB from './config/database';
 
 const server = new ApolloServer({
   typeDefs: Modules.typeDefs,
   resolvers: Modules.resolvers,
 });
+
+DB.on('error', console.error.bind(console, 'connection error:'));
 
 server.listen().then(({
   url,
