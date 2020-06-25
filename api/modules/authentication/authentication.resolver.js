680bffcc31677d0086e5bd06459dd3119c628e88
Shreyansh Zazz <shreyansh@notesss.com>
2020-06-25 22:03:43 +0530

    feat(authentication): user onboarding - registration (#23)
    Co-authored-by: Shreyansh Zazz <shreyansh@notesss.com>

--- api/modules/authentication/authentication.resolver.js
+++ api/modules/authentication/authentication.resolver.js
@@ -1,8 +1,32 @@
+import UserModel from '../../models/user.model'
+
 const AuthenticationResolver = {
   Query: {
     login: () => ({
       success: 'Success'
     })
+  },
+
+  Mutation: {
+    register: (parent, data, ctx) => {
+      const user = new UserModel(data)
+
+      var error = user.validateSync()
+      if (error) {
+        throw error
+      }
+
+      return {
+        code: 201,
+        success: true,
+        message: 'User added succefully',
+        user: {
+          first_name: 'asdsad',
+          last_name: 'adda',
+          primary_email: 'asdasd'
+        }
+      }
+    }
   }
 }
 
