e56abf8485e375225258d39bb722c0adc2e953d8
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-07-01 03:37:43 +0530

    fix: jest ignore build dir

--- jest.config.js
+++ jest.config.js
@@ -149,7 +149,8 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     '\\\\node_modules\\\\',
-    '\\\\build\\\\'
+    '\\\\build\\\\',
+    '<rootDir>/build'
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
