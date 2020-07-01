2accd1e2efd3666c6f5cf82103fff69d98e8a12a
Shreyansh Zazz <shreyansh@notesss.com>
2020-07-01 20:07:18 +0530

    test: add test related settings and new test (#38)
    * test: add unit test for validatoin methods
    
    * ci: add codecov for code coverage
    
    * ci: add path to coverage file
    
    * style: fix lint
    
    Co-authored-by: Shreyansh Zazz <shreyansh@notesss.com>

---
+++ api/config/test-config/database.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
+mongoose.promise = global.Promise
+
+async function removeAllCollections () {
+  const collections = Object.keys(mongoose.connection.collections)
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName]
+    await collection.deleteMany()
+  }
+}
+
+async function dropAllCollections () {
+  const collections = Object.keys(mongoose.connection.collections)
+  for (const collectionName of collections) {
+    const collection = mongoose.connection.collections[collectionName]
+    try {
+      await collection.drop()
+    } catch (error) {
+      if (error.message === 'ns not found') return
+      if (error.message.includes('a background operation is currently running')) return
+      console.log(error.message)
+    }
+  }
+}
+
+module.exports = {
+  setupDB (databaseName) {
+    // Connect to Mongoose
+    beforeAll(async () => {
+      await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+        console.log(`Database connect successfully: ${databaseName}`)
+      })
+    })
+
+    // Cleans up database between each test
+    afterEach(async () => {
+      await removeAllCollections()
+    })
+
+    // Disconnect Mongoose
+    afterAll(async () => {
+      await dropAllCollections()
+      await mongoose.connection.close()
+    })
+  }
+}
