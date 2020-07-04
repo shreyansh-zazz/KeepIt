c2d18adb42a4efff1a42d3115a9509a0c7089dcd
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-07-04 22:38:54 +0530

    fix: removed jest ignore

--- jest.config.js
+++ jest.config.js
@@ -24,9 +24,9 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    '\\\\config\\\\'
-  ],
+  // coveragePathIgnorePatterns: [
+  //   '\\\\config\\\\'
+  // ],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: 'babel',
